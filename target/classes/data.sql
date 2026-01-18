eb562d84a3d08e41091e8f5ea5efe0fa6d849294
Mert <mert.karacamm@gmail.com>
2026-01-18 23:55:02 +0300

    Reset cache

--- target/classes/data.sql
+++
@@ -1,7 +0,0 @@
--- Clear config table so as to avoid duplicate PK on every restart
-DELETE FROM operational_config;
-
-
-INSERT INTO operational_config (id, battery_charge_limit, voltage_window_minutes, voltage_change_percent)
-VALUES (1, 20.0, 5, 10.0);
-
